00dd142ef0715568037565df0515668e08bdc98c
fangchutao <fangchutao@126.com>
2018-07-30 17:40:23 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item in swiperList" :key="item.id">
+                <img :src="item.imgUrl" class="swiper-img"/>
+            </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".swiper-pagination"
+        },
+        loop:true
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl:
+            "https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/anquan_20180716.png"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/sc_sqdc_20180727.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="home">
        <home-header></home-header>
+       <home-swiper></home-swiper>
     </div>
 </template>
 
 <script>
 import homeHeader from './components/header'
+import homeSwiper from './components/Swiper'
 export default {
     name:"home",
     components:{
-        homeHeader
+        homeHeader,
+        homeSwiper
     }
 }
 </script>
